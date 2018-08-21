136b1c4e05486e427a422bfb79a649fc96163a39
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-21 19:49:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 document.addEventListener("DOMContentLoaded", e => {
   document.querySelector("body")
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
+ let values =document.getElementById('hidden-div').innerHTML='301';
+  
 })
