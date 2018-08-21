f01db9f3cc4b7932ed6b1d57c10227931baadb82
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-21 19:34:13 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ document.addEventListener("DOMContentLoaded", e => {
   .addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
 
-let values =document.
+let values =document.getElementByI
